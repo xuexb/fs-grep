1e3a79da0cbb616371c66552340b79ae8fb6dc25
xuexb <fe.xiaowu@gmail.com>
2016-04-03 11:50:28 +0800

    add: 添加命令行工具

---
+++ src/cli.js
@@ -0,0 +1,43 @@
+/**
+ * @file fs-grep cli
+ * @author xiaowu
+ * @email fe.xiaowu@gmail.com
+ */
+
+import program from 'commander';
+import 'colors';
+
+import pkg from '../package';
+import {exec} from '../';
+
+// 配置参数
+let option = {};
+
+// 设置命令行工具
+program
+    .version(pkg.version)
+    .arguments('<pattern> [files]')
+    .option('--color', '高亮')
+    .action((pattern, files) => {
+        option.pattern = pattern;
+        option.files = files || './**/*';
+    })
+    .parse(process.argv);
+
+if (!option.pattern) {
+    console.error('请使用 -h 查看帮助'.red);
+    process.exit(1);
+}
+
+let read = exec(option.pattern, option.files);
+
+read.on('line', (path, index, content) => {
+    if (program.color) {
+        console.log(path + ':' + index + ' => ', content.replace(new RegExp(option.pattern, 'g'), ($0) => {
+            return $0.red
+        }));
+    }
+    else {
+        console.log(path + ':' + index + ' => ' + content);
+    }
+});
